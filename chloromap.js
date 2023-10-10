f4f4369242bc097169c4b87587a21d96888915c7
Ryan Koh <ryankoh66@gmail.com>
2023-10-10 13:59:44 +0800

    new map with filter

--- chloromap.js
+++
@@ -1,4 +0,0 @@
-var vg_1  = "chloromap.json";
-vegaEmbed("#chloromap",vg_1).then(function(result){
-
-}).catch(console.error);
---
+++ co2_emissions_total.js
@@ -0,0 +1,4 @@
+var vg_1  = "co2_emissions_total.json";
+vegaEmbed("#co2_emissions_total",vg_1).then(function(result){
+
+}).catch(console.error);
